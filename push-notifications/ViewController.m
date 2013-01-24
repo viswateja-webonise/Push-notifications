8bebcf04703d2c3c3a4e75f120e2489445470f2d
Viswateja <viswateja.reddy@weboniselab.com>
2013-01-24 16:25:48 +0530

    initial commit

---
+++ push-notifications/ViewController.m
@@ -0,0 +1,48 @@
+//
+//  ViewController.m
+//  push-notifications
+//
+//  Created by Matt Vaznaian on 9/28/12.
+//  Copyright (c) 2012 StackMob. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "AppDelegate.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+@synthesize managedObjectContext = _managedObjectContext;
+
+- (AppDelegate *)appDelegate {
+    return (AppDelegate *)[[UIApplication sharedApplication] delegate];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.managedObjectContext = [self.appDelegate managedObjectContext];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+- (IBAction)registerDevice:(id)sender {
+    
+    [[UIApplication sharedApplication] registerForRemoteNotificationTypes:UIRemoteNotificationTypeAlert];
+    
+}
+
+@end
